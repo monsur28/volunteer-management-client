aa8ac911e5b6baeed2f01d1baab18cad4e89d1ed
Abul Monsur <154686044+monsur28@users.noreply.github.com>
2024-05-13 01:44:53 +0600

    Form Created

--- src/Pages/AddVolunteerPost/AddVolunteerPost.jsx
+++ src/Pages/AddVolunteerPost/AddVolunteerPost.jsx
@@ -1,4 +1,34 @@
+import { useContext, useState } from "react";
+import ReactDatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { AuthContext } from "../../Providers/AuthProvider";
+
 const AddVolunteerPost = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const { user } = useContext(AuthContext);
+  const handleAddPost = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const thumbnail = form.thumbnail.value;
+    const posttitle = form.posttitle.value;
+    const description = form.description.value;
+    const category = form.category.value;
+    const location = form.location.value;
+    const date = form.date.value;
+    const name = user?.email;
+    const email = user?.email;
+    const newVoluteerPost = {
+      thumbnail,
+      posttitle,
+      description,
+      category,
+      location,
+      date,
+      name,
+      email,
+    };
+    console.log(newVoluteerPost);
+  };
   return (
     <div>
       <h2 className="text-center text-5xl font-bold underline">
@@ -6,8 +36,7 @@ const AddVolunteerPost = () => {
       </h2>
       <section className="p-6 dark:bg-gray-100 dark:text-gray-900">
         <form
-          noValidate=""
-          action=""
+          onClick={handleAddPost}
           className="container flex flex-col mx-auto space-y-12"
         >
           <fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm dark:bg-gray-50">
@@ -29,6 +58,7 @@ const AddVolunteerPost = () => {
                 <input
                   id="thumbnail"
                   type="text"
+                  name="thumbnail"
                   placeholder="Thumbnail"
                   className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300"
                 />
@@ -40,6 +70,7 @@ const AddVolunteerPost = () => {
                 <input
                   id="posttitle"
                   type="text"
+                  name="posttitle"
                   placeholder="Post Title"
                   className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300"
                 />
@@ -48,6 +79,7 @@ const AddVolunteerPost = () => {
                 <label className="text-sm">Description</label>
                 <textarea
                   id="description"
+                  name="description"
                   type="text"
                   placeholder="Description"
                   className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300"
@@ -69,7 +101,7 @@ const AddVolunteerPost = () => {
               </div>
               <div className="col-span-full sm:col-span-3">
                 <label htmlFor="city" className="text-sm">
-                  City
+                  Location
                 </label>
                 <input
                   id="city"
@@ -79,34 +111,30 @@ const AddVolunteerPost = () => {
                   className="w-full p-4 mt-2 rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-500 focus:dark:ring-violet-600 border border-black dark:border-gray-300"
                 />
               </div>
-              <div className="col-span-full sm:col-span-2">
-                <label htmlFor="state" className="text-sm">
-                  State / Province
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="date" className="text-sm">
+                  Date
                 </label>
-                <input
-                  id="state"
-                  type="text"
-                  placeholder=""
-                  className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300"
-                />
-              </div>
-              <div className="col-span-full sm:col-span-2">
-                <label htmlFor="zip" className="text-sm">
-                  ZIP / Postal
-                </label>
-                <input
-                  id="zip"
-                  type="text"
-                  placeholder=""
-                  className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300"
+                <br />
+                <ReactDatePicker
+                  showIcon
+                  name="date"
+                  selected={startDate}
+                  onChange={(date) => setStartDate(date)}
+                  className="w-full p-4 mt-2 rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-500 focus:dark:ring-violet-600 border border-black dark:border-gray-300"
                 />
               </div>
             </div>
           </fieldset>
           <fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm dark:bg-gray-50">
             <div className="space-y-2 col-span-full lg:col-span-1">
-              <p className="font-medium">Profile</p>
-              <p className="text-xs">Adipisci fuga autem eum!</p>
+              <p className="font-medium text-xl">Organizer Information</p>
+              <p className="text-base">
+                Welcome to the Organizer Profile section! This is your space to
+                showcase your involvement, contributions, and passion for
+                organizing volunteer opportunities and making a difference in
+                your community.
+              </p>
             </div>
             <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
               <div className="col-span-full sm:col-span-3">
@@ -116,46 +144,33 @@ const AddVolunteerPost = () => {
                 <input
                   id="username"
                   type="text"
+                  name="name"
+                  defaultValue={user.displayName}
+                  readOnly
                   placeholder="Username"
                   className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300"
                 />
               </div>
               <div className="col-span-full sm:col-span-3">
                 <label htmlFor="website" className="text-sm">
-                  Website
+                  Email
                 </label>
                 <input
-                  id="website"
-                  type="text"
-                  placeholder="https://"
+                  id="email"
+                  type="email"
+                  defaultValue={user.email}
+                  readOnly
+                  placeholder="Email"
                   className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300"
                 />
               </div>
               <div className="col-span-full">
-                <label htmlFor="bio" className="text-sm">
-                  Bio
-                </label>
-                <textarea
-                  id="bio"
-                  placeholder=""
-                  className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300"
-                ></textarea>
-              </div>
-              <div className="col-span-full">
-                <label htmlFor="bio" className="text-sm">
-                  Photo
-                </label>
                 <div className="flex items-center space-x-2">
-                  <img
-                    src="https://source.unsplash.com/30x30/?random"
-                    alt=""
-                    className="w-10 h-10 dark:bg-gray-500 rounded-full dark:bg-gray-300"
-                  />
                   <button
                     type="button"
-                    className="px-4 py-2 border rounded-md dark:border-gray-800"
+                    className="btn btn-block bg-green-400 text-black text-wpx-4 py-2 border rounded-md dark:border-gray-800"
                   >
-                    Change
+                    Add Post
                   </button>
                 </div>
               </div>
